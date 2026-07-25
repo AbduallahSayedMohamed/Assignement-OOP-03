414ee8a7f79d606dc30f4e0b45064694d86ae286
AbduallahSayedMohamed <abdallahsayedmohamed010@gmail.com>
2026-07-25 07:18:41 +0300

    Question-01

--- Assignement OOP-03/Program.cs
+++ Assignement OOP-03/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
 
+            #region Question 1
+            /* 
+             * Q1 :
+             * Identify the type of relationship in each scenario below 
+             * (Inheritance, Association, Aggregation, Composition, or Dependency):
+             * --------------------------------------------------------------------
+             * a) A University has Departments. If the university is closed, the departments no longer exist?
+             * -----> Composition Relationship (HAS-A – Strong)
+             * b) A Driver uses a Car. The driver does not own the car?
+             * -----> Assiciation Relationship 
+             * c) A Dog is an Animal.
+             * -----> Inheritance IS-A 
+             * d) A Team has Players. If the team is deleted, the players still exist.
+             * -----> Aggregation (Has-A) Weak.
+             * e) A method receives a Logger as a parameter and calls it inside the method only.
+             * -----> Dependency Relationship
+             */
+            #endregion
+
         }
     }
 }
